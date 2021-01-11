4a666f6ddf5645c76f6593f59ff5b4bf6cfebdb6
racheallarimer <racheal.larimer@gmail.com>
2021-01-11 14:28:44 -0800

    connect projectHandler to index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import events from "./eventsBus"
-import projectHandler from "./projectHandler"
+import initProjectHandler from "./projectHandler"
+
+initProjectHandler();
+
+(function eventHandlers() {
+  const addBtn = document.getElementById('add');
+console.log(addBtn)
+  addBtn.addEventListener('click', () => {
+    events.emit('addBook', [1,2,3,4,['a', 'b']]);
+    console.log('emitting')
+  });
+})();
--- src/projectHandler.js
+++ src/projectHandler.js
@@ -1,20 +1,30 @@
 import events from "./eventsBus";
 
-function projectHandler() {
-  let projects = [];
+function initProjectHandler() {
+  (() => {
+    events.on('addBook', createNewProject)
+  })();
+}
+
+let projects = [];
 
   class Project {
     constructor(title, description, dueDate, priority, notes, tasks) {
-      title;
-      description;
-      dueDate;
-      priority;
-      notes;
-      tasks;
+      this.title = title;
+      this.description = description;
+      this.dueDate = dueDate;
+      this.priority = priority;
+      this.notes = notes;
+      this.tasks = tasks;
     }
   }
 
+  const createNewProject = (data) => {
+    console.log('creatingBook')
+    let newProject = new Project(data[0], data[1], data[2], data[3], data[4], data[5]);
+    projects.push(newProject);
+    console.log(newProject);
+  }
 
-}
 
-export default projectHandler;
+export default initProjectHandler;
