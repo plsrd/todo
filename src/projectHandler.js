07b467d69f27fc15bd28821fef29b6d15b136a1c
racheallarimer <racheal.larimer@gmail.com>
2021-01-12 12:48:07 -0800

    add getExstingProjects to projectHandler

--- src/projectHandler.js
+++ src/projectHandler.js
@@ -1,10 +1,17 @@
 import events from "./eventsBus";
 
 function initProjectHandler() {
-  events.on('createProject', createNewProject)
+  events.on('createProject', createNewProject);
+  getExistingProjects();
 }
 
 let projects = [];
+
+function getExistingProjects() {
+  if (projects[0] !== undefined) {
+    events.emit('existingProjects', projects);
+  }
+}
 
 class Project {
   constructor(title, description, dueDate, priority, notes) {
@@ -19,7 +26,6 @@ class Project {
 export const createNewProject = (data) => {
   let newProject = new Project(data[0], data[1], data[2], data[3], data[4]);
   projects.push(newProject);
-  console.log(projects);
 }
 
 
